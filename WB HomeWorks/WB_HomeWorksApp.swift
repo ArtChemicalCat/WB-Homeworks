94033ac024e93880405ae25ded4fdc273fbbf844
Nikolay Kazanin <artchemist@yandex.ru>
2024-07-15 06:07:39 +0300

    Initial Commit

---
+++ WB HomeWorks/WB_HomeWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WB_HomeWorksApp.swift
+//  WB HomeWorks
+//
+//  Created by Николай Казанин on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WB_HomeWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
